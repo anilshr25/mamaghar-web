bf0607f5684c45c2a8f4ed5200366cbedcb87e73
pratik bhattarai <bhattaraipratik663@gmail.com>
2023-10-16 21:40:36 +0545

    Footer-Fixed

--- resources/views/front/layouts/footer.blade.php
+++ resources/views/front/layouts/footer.blade.php
@@ -2,16 +2,17 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3 text-center text-md-left">
-                <img src="{{ getSiteSetting()->footer_logo_path['original'] ?? asset('front_assets/img/logo.png') }}" alt="">
+                <img src="{{ getSiteSetting()->footer_logo_path['original'] ?? asset('front_assets/img/logo.png') }}"
+                    alt="">
                 <p class="color-grey mt-4">
                     {{ getSiteSetting()->address ?? 'Kathmandu' }}<br>{{ getSiteSetting()->city ?? 'Kathmandu' }}</p>
             </div>
             <div class="col-md-4 text-center text-md-left">
                 <h6 class="color-white mb-3">information</h6>
-                <a href="room">Rooms</a>
-                <a href="services">Services</a>
-                <a href="restaurant">Restaurant</a>
-                <a href="faq">Faq</a>
+                <a href="{{ route('front.room') }}">Rooms</a>
+                <a href="{{ route('front.room') }}">Services</a>
+                <a href="{{ route('front.restaurant') }}">Restaurant</a>
+                <a href="{{ route('front.restaurant') }}">Faq</a>
                 <a href="#">Gallery & images</a>
             </div>
             <div class="col-md-5 mt-4 mt-md-0 text-center text-md-left logos-footer">
@@ -30,17 +31,17 @@
                 <p>2023 © Mamaghar. All rights reserved.</p>
             </div>
             @if (getSiteSetting() && (getSiteSetting()->facebook || getSiteSetting()->twitter || getSiteSetting()->instagram))
-            <div class="col-md-6 text-center text-md-right">
-                @if (getSiteSetting() && getSiteSetting()->facebook)
-                    <a href="{{ getSiteSetting()->facebook }}" class="social-footer-bottom">Facebook</a>
-                @endif
-                @if (getSiteSetting() && getSiteSetting()->twitter)
-                    <a href="{{ getSiteSetting()->twitter }}" class="social-footer-bottom">Twitter</a>
-                @endif
-                @if (getSiteSetting() && getSiteSetting()->instagram)
-                    <a href="{{ getSiteSetting()->instagram }}" class="social-footer-bottom">Instagram</a>
-                @endif
-            </div>
+                <div class="col-md-6 text-center text-md-right">
+                    @if (getSiteSetting() && getSiteSetting()->facebook)
+                        <a href="{{ getSiteSetting()->facebook }}" class="social-footer-bottom">Facebook</a>
+                    @endif
+                    @if (getSiteSetting() && getSiteSetting()->twitter)
+                        <a href="{{ getSiteSetting()->twitter }}" class="social-footer-bottom">Twitter</a>
+                    @endif
+                    @if (getSiteSetting() && getSiteSetting()->instagram)
+                        <a href="{{ getSiteSetting()->instagram }}" class="social-footer-bottom">Instagram</a>
+                    @endif
+                </div>
             @endif
         </div>
     </div>
