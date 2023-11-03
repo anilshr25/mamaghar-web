e7753fc5d2eedd928ff338d6a58d54c92c7e678d
pratik bhattarai <bhattaraipratik663@gmail.com>
2023-11-03 15:12:41 +0545

    removed-overview

--- resources/views/front/adventure/adventure.blade.php
+++ resources/views/front/adventure/adventure.blade.php
@@ -20,4 +20,33 @@
             </div>
         </div>
     </div>
+
+    <div class="section padding-top-bottom z-bigger">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 align-self-center">
+                    <div class="subtitle with-line text-center mb-4">Thrilling</div>
+                    <h3 class="text-center padding-bottom-small">Adventure</h3>
+                </div>
+                <div class="section clearfix"></div>
+                @foreach ($adventures as $adventure)
+                    <div class="col-md-6 mt-5" data-scroll-reveal="enter bottom move 50px over 0.7s after 0.2s">
+                        <div class="restaurant-box">
+                            @if ($adventure && $adventure->image_path)
+                                <img src="{{ $adventure->image_path['original'] }}" alt="{{ $adventure['title'] }}">
+                            @else
+                                <img src="{{ asset('front_assets/img/room3.jpg') }}" alt="{{ $adventure['title'] }}">
+                            @endif
+                            <h6><span>{{ $adventure->title }}</span></h6>
+                            @if ($adventure->category)
+                                <p><span>{{ $adventure->category->title ?? '' }}</span></p>
+                            @endif
+
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+    </div>
 @endsection
