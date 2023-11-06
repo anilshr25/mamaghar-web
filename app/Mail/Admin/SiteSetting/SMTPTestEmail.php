a6634c7136b01c46ec9765f45cc896c5121bba5d
Anil Shrestha <31877452+anilshr25@users.noreply.github.com>
2023-11-06 20:08:18 +0545

    fix test mail

--- app/Mail/Admin/SiteSetting/SMTPTestEmail.php
+++ app/Mail/Admin/SiteSetting/SMTPTestEmail.php
@@ -4,8 +4,6 @@ namespace App\Mail\Admin\SiteSetting;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class SMTPTestEmail extends Mailable
@@ -13,24 +11,12 @@ class SMTPTestEmail extends Mailable
     use Queueable, SerializesModels;
 
 
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
+    public function build()
     {
-        return new Envelope(
-            subject: 'SMTP Test Email',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'emails.site-setting.test-email',
-        );
+        $setting = getSiteSetting();
+        return $this->view('emails.site-setting.test-email')
+            ->subject("Test email")
+            ->from($setting->mail_from_address, $setting->mail_sender_name);
     }
 
 }
